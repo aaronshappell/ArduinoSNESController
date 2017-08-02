181a417c1dd5d1b8b72fc3b823ce7d68170ed51a
aaronshappell <crumster88@gmail.com>
2017-08-02 13:30:36 +0300

    Add button methods

--- ArduinoSNESController.cpp
+++ ArduinoSNESController.cpp
@@ -31,9 +31,153 @@ bool ArduinoSNESController::isButtonUp(int button){
 }
 
 void ArduinoSNESController::update(){
-    
+    buttonsOld = buttons;
 }
 
 word ArduinoSNESController::getButtons(){
+    return buttons;
+}
 
+bool ArduinoSNESController::isButtonPressedB(){
+    isButtonPressed(0);
+}
+
+bool ArduinoSNESController::isButtonPressedY(){
+    isButtonPressed(1);
+}
+
+bool ArduinoSNESController::isButtonPressedSelect(){
+    isButtonPressed(2);
+}
+
+bool ArduinoSNESController::isButtonPressedStart(){
+    isButtonPressed(3);
+}
+
+bool ArduinoSNESController::isButtonPressedUp(){
+    isButtonPressed(4);
+}
+
+bool ArduinoSNESController::isButtonPressedDown(){
+    isButtonPressed(5);
+}
+
+bool ArduinoSNESController::isButtonPressedLeft(){
+    isButtonPressed(6);
+}
+
+bool ArduinoSNESController::isButtonPressedRight(){
+    isButtonPressed(7);
+}
+
+bool ArduinoSNESController::isButtonPressedA(){
+    isButtonPressed(8);
+}
+
+bool ArduinoSNESController::isButtonPressedX(){
+    isButtonPressed(9);
+}
+
+bool ArduinoSNESController::isButtonPressedL(){
+    isButtonPressed(10);
+}
+
+bool ArduinoSNESController::isButtonPressedR(){
+    isButtonPressed(11);
+}
+
+bool ArduinoSNESController::isButtonDownB(){
+    isButtonDown(0);
+}
+
+bool ArduinoSNESController::isButtonDownY(){
+    isButtonDown(1);
+}
+
+bool ArduinoSNESController::isButtonDownSelect(){
+    isButtonDown(2);
+}
+
+bool ArduinoSNESController::isButtonDownStart(){
+    isButtonDown(3);
+}
+
+bool ArduinoSNESController::isButtonDownUp(){
+    isButtonDown(4);
+}
+
+bool ArduinoSNESController::isButtonDownDown(){
+    isButtonDown(5);
+}
+
+bool ArduinoSNESController::isButtonDownLeft(){
+    isButtonDown(6);
+}
+
+bool ArduinoSNESController::isButtonDownRight(){
+    isButtonDown(7);
+}
+
+bool ArduinoSNESController::isButtonDownA(){
+    isButtonDown(8);
+}
+
+bool ArduinoSNESController::isButtonDownX(){
+    isButtonDown(9);
+}
+
+bool ArduinoSNESController::isButtonDownL(){
+    isButtonDown(10);
+}
+
+bool ArduinoSNESController::isButtonDownR(){
+    isButtonDown(11);
+}
+
+bool ArduinoSNESController::isButtonUpB(){
+    isButtonUp(0);
+}
+
+bool ArduinoSNESController::isButtonUpY(){
+    isButtonUp(1);
+}
+
+bool ArduinoSNESController::isButtonUpSelect(){
+    isButtonUp(2);
+}
+
+bool ArduinoSNESController::isButtonUpStart(){
+    isButtonUp(3);
+}
+
+bool ArduinoSNESController::isButtonUpUp(){
+    isButtonUp(4);
+}
+
+bool ArduinoSNESController::isButtonUpDown(){
+    isButtonUp(5);
+}
+
+bool ArduinoSNESController::isButtonUpLeft(){
+    isButtonUp(6);
+}
+
+bool ArduinoSNESController::isButtonUpRight(){
+    isButtonUp(7);
+}
+
+bool ArduinoSNESController::isButtonUpA(){
+    isButtonUp(8);
+}
+
+bool ArduinoSNESController::isButtonUpX(){
+    isButtonUp(9);
+}
+
+bool ArduinoSNESController::isButtonUpL(){
+    isButtonUp(10);
+}
+
+bool ArduinoSNESController::isButtonUpR(){
+    isButtonUp(11);
 }
--- ArduinoSNESController.h
+++ ArduinoSNESController.h
@@ -27,6 +27,42 @@ class ArduinoSNESController{
         ArduinoSNESController(int _latchPin, int _clockPin, int _dataPin);
         void update();
         word getButtons();
+        bool isButtonPressedB();
+        bool isButtonPressedY();
+        bool isButtonPressedSelect();
+        bool isButtonPressedStart();
+        bool isButtonPressedUp();
+        bool isButtonPressedDown();
+        bool isButtonPressedLeft();
+        bool isButtonPressedRight();
+        bool isButtonPressedA();
+        bool isButtonPressedX();
+        bool isButtonPressedL();
+        bool isButtonPressedR();
+        bool isButtonDownB();
+        bool isButtonDownY();
+        bool isButtonDownSelect();
+        bool isButtonDownStart();
+        bool isButtonDownUp();
+        bool isButtonDownDown();
+        bool isButtonDownLeft();
+        bool isButtonDownRight();
+        bool isButtonDownA();
+        bool isButtonDownX();
+        bool isButtonDownL();
+        bool isButtonDownR();
+        bool isButtonUpB();
+        bool isButtonUpY();
+        bool isButtonUpSelect();
+        bool isButtonUpStart();
+        bool isButtonUpUp();
+        bool isButtonUpDown();
+        bool isButtonUpLeft();
+        bool isButtonUpRight();
+        bool isButtonUpA();
+        bool isButtonUpX();
+        bool isButtonUpL();
+        bool isButtonUpR();
 }
 
 #endif
