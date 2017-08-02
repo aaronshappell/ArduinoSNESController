082edeaacf5e739b8bd56f1178ccf002cbeebed6
aaronshappell <crumster88@gmail.com>
2017-08-02 11:19:58 +0300

    Update example

--- examples/ReadController/ReadController.ino
+++ examples/ReadController/ReadController.ino
@@ -1,7 +1,40 @@
-void setup(){
+/*
+    ReadController.ino - Example for ArduinoSNESController library
+    Created by Aaron Shappell
+*/
 
+#include <ArduinoSNESController.h>
+
+//Define pins
+int latchPin = 9;
+int clockPin = 10;
+int dataPin = 11;
+
+ArduinoSNESController controller(latchPin, clockPin, dataPin);
+
+void setup(){
+    Serial.begin(9600);
 }
 
 void loop(){
-    
+    //Update the controller
+    controller.update();
+
+    //Check if a button has been pressed
+    if(controller.isButtonPressedA()){
+        Serial.println("Button A has been Pressed!");
+    }
+
+    //Check if a button is down
+    if(controller.isButtonDownA()){
+        Serial.println("Button A is down!");
+    }
+
+    //Check if a button is up
+    if(controller.isButtonUpA()){
+        Serial.println("Button A is up!");
+    }
+
+    //Get word that represents all button states
+    word buttons = controller.getButtons();
 }
